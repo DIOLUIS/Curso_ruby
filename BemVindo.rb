8802851b9a7809c55f3fec843469b5b672924d7c
DIOLUIS <diogoparazita@gmail.com>
2021-04-30 05:46:53 -0300

    Diferença entre puts e print

---
+++ BemVindo.rb
@@ -0,0 +1,9 @@
+puts ('Entre com seu nome: ')#Exibe um texto ao usuário 
+nome = gets()#Atribui o dado digitado a variável nome
+puts ("Bem-Vindo, #{nome}")#Imprime na tela "Bem vindo e contatena a variável nome"
+
+
+=begin
+print -> Comando que não quebra linha
+puts  -> Comando que quebra linha
+=end
