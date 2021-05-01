17dfbfaa2f5ec2138010d79d8a459bdc49191f59
DIOLUIS <diogoparazita@gmail.com>
2021-05-01 10:36:39 -0300

    Usando if and else

---
+++ Idade.rb
@@ -0,0 +1,12 @@
+puts("Programa de primeiro emprego")
+puts("Quantos anos você tem? ")
+idade = gets() 
+
+if(idade.to_i >= 18) #se("a idade digitada pelo usuário maior ou ihual a 18")
+    then #então faça
+    puts("Você poderá trabalhar.")
+
+    else #senão for atendido a expressao da função
+    puts("Você não tem idade para começar a trabalhar.")
+    
+    end #fim da função
