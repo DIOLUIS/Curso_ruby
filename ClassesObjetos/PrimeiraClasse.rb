7904f6926e64e54df89ac0cdaf74f9221ced3029
DIOLUIS <diogoparazita@gmail.com>
2021-05-01 11:11:49 -0300

    FirstClass

---
+++ ClassesObjetos/PrimeiraClasse.rb
@@ -0,0 +1,6 @@
+class Aula #definição da classe sempre primeira letra maiúscula
+    #Corpo da classe
+end #Fim da classe
+
+aula = Aula.new #Cria um objeto usando a classe
+puts aula.class #Exibe o nome da classe do objeto
